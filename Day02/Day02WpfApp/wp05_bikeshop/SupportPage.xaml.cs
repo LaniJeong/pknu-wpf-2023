11fb4caff13b6adfbf6b4f93c9deb77582a90a3c
LaniJeong <123913969+LaniJeong@users.noreply.github.com>
2023-04-19 17:25:19 +0900

    4월 19일_1차 저장

---
+++ Day02/Day02WpfApp/wp05_bikeshop/SupportPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using wp05_bikeshop.logics;
+
+namespace wp05_bikeshop
+{
+    /// <summary>
+    /// SupportPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SupportPage : Page
+    {
+        public SupportPage()
+        {
+            InitializeComponent();
+            InitializeComponent();
+        }
+
+        private void InitCar()
+        {
+            // 일반적 C#에서 클래스 객체 인스턴스 사용방법 동일
+            Car myCar = new Car();
+            myCar.Names = "아이오닉";
+        }
+
+    }
+}
